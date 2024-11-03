a2dced877fb64a69b3e8e63d45091927df0b9d97
Fiqry-Wahyu-Diky <fiqridiky01@gmail.com>
2024-11-03 14:18:33 +0530

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -215,6 +215,6 @@ with tab3:
 
     with st.expander("See Explanation"):
         st.write(
-            'Sesuai dengan grafik yang sudah dibuat, ada lebih banyak pelanggan di bagian tenggara dan selatan. Informasi lainnya, ada lebih banyak pelanggan di kota-kota yang merupakan ibu kota (São Paulo, Rio de Janeiro, Porto Alegre, dan lainnya).')
+            'Sesuai dengan grafik yang sudah dibuat, ada lebih banyak pelanggan di bagian tenggara dan selatan. Informasi lainnya, ada lebih banyak pelanggan di kota-kota yang merupakan ibu kota (São Paulo, Rio de Janeiro, Porto Alegre, dan lainnya) haduh.')
 
 
